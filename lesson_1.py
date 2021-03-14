7a1d91937051d6feca2fd522c7980dc67fa3494a
grimraven64 <morkovinav@gmail.com>
2021-03-14 12:58:58 +0400

    lesson_1.py added

---
+++ lesson_1.py
@@ -0,0 +1,31 @@
+#1.
+
+print('Hello World')
+
+
+
+#2.
+
+print ('Hello World')
+
+
+
+#3.
+
+print('Hello World')
+
+
+
+#4.
+
+print ('Hello')
+
+
+
+#5.
+
+print('Привет',',')
+
+print ('Меня зовут Вася')
+
+print ('и я умнее всех')
